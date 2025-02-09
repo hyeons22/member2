bafde1d31d5c80542e4d10fc242b1b2556849e3c
hyeon <ski0123@naver.com>
2025-02-09 10:39:12 +0900

    first commit

---
+++ src/main/java/com/example/memberpractice2/MemberPractice2Application.java
@@ -0,0 +1,13 @@
+package com.example.memberpractice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberPractice2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberPractice2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/memberpractice2/MemberPractice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memberpractice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberPractice2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
